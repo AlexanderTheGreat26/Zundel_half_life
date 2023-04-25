9d619b348229efd85ee078d125c095fde22854c2
AlexanderTheGreat26 <kaegod@yandex.ru>
2023-04-25 23:19:31 +0300

    analyze

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@
 #include <string>
 #include <tuple>
 #include <sstream>
+#include <numeric>
 
 
 const int T = 100; // Frame period.
+const double timestep = 0.25; // fs
+
+
 
 
 typedef std::tuple<int, int, int, int, int> cation; // <atom_ID, atom_type, bond_count, first_bonded_atom_ID, second_bonded_atom_ID>
 
 typedef std::vector<std::vector<std::string>> cations;
+
+typedef std::vector<std::string> frame;
+
 
 
 cations cations_strings (const std::string & file_name, const std::string & pattern);
@@ -20,6 +27,9 @@ void file_creation (const std::string & file_name, cations & stuff, const int & 
 
 cations Zundels_remove (cations & Zundels, cations & stuff, const int & frame_period);
 
+std::vector<int> uniq_lifes (cations & cation_frames);
+
+#include <algorithm>
 
 int main() {
     // We need find different Zundel cations. Their sign is string " 2 2 " between atoms numbers.
@@ -27,16 +37,28 @@ int main() {
     file_creation("Zundels.only", Zundels, T);
     // Then we need H3O-cations, but in default file we can find parts of Z-cations with pattern " 3 3 ".
     cations stuff = std::move(cations_strings("bonds.only", " 3 3 "));
-    //file_creation("stuff", stuff, T);
+    // file_creation("stuff", stuff, T);
     // Then need remove parts of this Z-cations from corresponding frames and find uniq H3O-cations.
     // For this purpose we will read the stuff-file and use type cation.
     cations H3O = std::move(Zundels_remove(Zundels, stuff, T));
     file_creation("H3O.only", H3O, T);
+    // Then we want to know, how long our particles lives.
+
+    std::vector<int> Zundel_times = std::move(uniq_lifes(Zundels));
+    std::vector<int> H3O_times = std::move(uniq_lifes(H3O));
+
+    int longest_Zundel_life = *std::max_element(Zundel_times.begin(), Zundel_times.end());
+    int longest_H3O_life = *std::max_element(H3O_times.begin(), H3O_times.end());
+
+    //std::generate()
 
 
 
     return 0;
 }
+
+
+
 
 
 void file_creation (const std::string & file_name, cations & stuff, const int & step) {
@@ -50,6 +72,39 @@ void file_creation (const std::string & file_name, cations & stuff, const int & 
         fout.close();
 }
 
+
+bool any_of (std::string & current_cation, std::vector<std::string> & cation_frame, int & index) {
+    for (int i = 0; i < cation_frame.size(); ++i)
+        if (current_cation == cation_frame[i]) {
+            index = i;
+            return true;
+        }
+    return false;
+}
+
+
+int alive_for (std::string & uniq_cation, cations & cation_frames, int & frame_number) {
+    int cation_in_frame_pos, lifetime = 0;
+    for (int i = frame_number; i < cation_frames.size(); ++i) {
+        if (any_of(uniq_cation, cation_frames[i], cation_in_frame_pos)) {
+            cation_frames[i].erase(cation_frames[i].begin()+cation_in_frame_pos);
+            ++lifetime;
+        } else
+            break;
+    }
+    return lifetime;
+}
+
+
+std::vector<int> uniq_lifes (cations & cation_frames) {
+    std::vector<int> result;
+    std::string cation;
+    for (int i = 0; i < cation_frames.size(); ++i)
+        while (!cation_frames[i].empty())
+            for (int j = 0; j < cation_frames[i].size(); ++j)
+                result.emplace_back(alive_for(cation_frames[i][j], cation_frames, i));
+    return result;
+}
 
 
 bool contain (const std::string & word, const std::string & line) {
